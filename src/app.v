cf728959485e8128b970ba3a49afc9cb43a84003
billpcs <bpanagio@protonmail.com>
2023-12-21 15:58:15 +0200

    hotfix to make server working again

--- src/app.v
+++ src/app.v
@@ -104,3 +104,43 @@ fn (mut app App) init_server() {
 	app.mount_static_folder_at(os.resource_abs_path('./img'), '/img')
 }
 
+pub fn (mut app App) index() vweb.Result {
+       app.debug("user '${app.ip()}' requested index")
+       return $vweb.html()
+}
+
+pub fn (mut app App) about() vweb.Result {
+       app.debug("user '${app.ip()}' requested about")
+       return $vweb.html()
+}
+
+pub fn (mut app App) notfound() vweb.Result {
+       app.debug("user '${app.ip()}' went to notfound")
+       app.set_status(404, 'Not Found')
+       return $vweb.html()
+}
+
+@['/tags']
+pub fn (mut app App) tags() vweb.Result {
+	return $vweb.html()
+}
+
+@['/']
+pub fn (mut app App) slash() vweb.Result {
+       return app.index()
+}
+
+@['/:post']
+pub fn (mut app App) post(name string) vweb.Result {
+       post := app.find_post_by_name(name) or {
+               return app.notfound()
+       }
+       app.debug("user '${app.ip()}' accessed post '${name}'")
+       post_title := post.title
+       post_url := post.url
+       chunks := post.text
+       tags := post.tags
+       return $vweb.html()
+}
+
+
